049eb210274fdb7b887294a913dfd646f26eae82
KristianHeggdal <kristianheggdal@hotmail.no>
2024-11-18 13:48:13 +0100

    Add project files.

---
+++ Variabeltilordning oppgave 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number1 = 5;
+int number2 = 6;
+
+int returnSum(int number1, int number2)
+{
+    return number1 + number2;
+}
+
+Console.WriteLine($"Summen av tallene er:{returnSum(number1,number2)}");
+
+
